04e0ef30c74767b108ab866a7c2e95da84873b0b
Danial <danialhakim01@gmail.com>
2021-04-24 12:16:01 +0100

    Project added

--- Gameloop/js/script.js
+++ Gameloop/js/script.js
@@ -9,8 +9,6 @@ var canvas = document.getElementById("game");
 // get 2D context for this canvas
 var context = canvas.getContext("2d");
 
-var score = 0;
-
 function GameObject(name, img, health) {
     this.name = name;
     this.img = img;
@@ -19,147 +17,54 @@ function GameObject(name, img, health) {
     this.y = 0;
 }
 
-function drawProgressBar(val) {
-    var width = 100;
-    var height = 20;
-    var max = 100;
-
-    // Draw the background
-    context.fillStyle = "#000000";
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.fillRect(0, 0, width, height);
-  
-    // Draw the fill
-    context.fillStyle = "#00FFFF";
-    var fillVal = Math.min(Math.max(val / max, 0), 1);
-    context.fillRect(0, 0, fillVal * width, height);
-  }
-
-
 // Sprite
 var sprite = new Image();
-sprite.src = "./img/running.png"; // Frames 1 to 6
-var npcSprite = new Image();
-npcSprite.src ="./img/goal.png"
+sprite.src = "./img/playerOne.png"; 
+
+var enemyNPCSprite = new Image();
+enemyNPCSprite.src = "./img/enemyOne.png";
+
+var backgroundSprite = new Image();
+backgroundSprite.src = "./img/background500.png";
+
 // The GamerInput is an Object that holds the Current
 // GamerInput (Left, Right, Up, Down)
 function GamerInput(input) {
     this.action = input;
 }
 
+function EnemyInput(enemyInput) {
+    this.action = enemyInput;
+}
+
 // Default GamerInput is set to None
 var gamerInput = new GamerInput("None"); //No Input
+
+// enemy input
+var enemyInput = new EnemyInput("None");
 
 // Default Player
 var player = new GameObject("Player",sprite, 100);
 
+var enemyNPC = new GameObject("enemyNPC",enemyNPCSprite,100);
+
 // Gameobjects is a collection of the Actors within the game
-var gameobjects = [player, new GameObject("NPC", npcSprite, 100)];
+var gameobjects = [player,enemyNPC];
 
-gameobjects[0].x = 100;
-gameobjects[0].y = 100;
+gameobjects[0].x = 20;
+gameobjects[0].y = 50;
 
-gameobjects[1].x = 400;
-gameobjects[1].y = 400;
+gameobjects[1].x = 180;
+gameobjects[1].y = 50;
+
 // Process keyboard input event
 function input(event) {
     // Take Input from the Player
-       console.log("Event type: " + event.type);
-    if (event.type === "keydown") {
-        switch (event.keyCode) {
-            case 37:
-                gamerInput = new GamerInput("Left");
-                break; //Left key
-            case 38:
-                gamerInput = new GamerInput("Up");
-                break; //Up key
-            case 39:
-                gamerInput = new GamerInput("Right");
-                break; //Right key
-            case 40:
-                gamerInput = new GamerInput("Down");
-                break; //Down key
-            default:
-                gamerInput = new GamerInput("None"); //No Input
-        }
-    } else {
-        gamerInput = new GamerInput("None"); //No Input
-    }
     console.log("Gamer Input :" + gamerInput.action);
 }
 
 function update() {
-    // Iterate through all GameObjects
-    // Updating position and gamestate
-    
-        if (gamerInput.action === "Up") {
-            gameobjects[0].y -= 5;
-            updatePosition();
-            console.log("Up");
-        }
-
-        if (gamerInput.action === "Down"){
-            gameobjects[0].y += 5;
-            updatePosition();
-            console.log("Down");
-        }
-
-        if(gamerInput.action === "Left"){
-           gameobjects[0].x -= 5;
-           updatePosition();
-           console.log("Left");
-        }
-
-        if(gamerInput.action === "Right"){
-            gameobjects[0].x += 5;
-            updatePosition();
-            console.log("Right");
-        }
-
-        if((gameobjects[0].x === gameobjects[1].x + 50) || (gameobjects[0].x === gameobjects[1].x - 50))
-        { 
-            if((gameobjects[0].y === gameobjects[1].y + 50) || (gameobjects[0].y === gameobjects[1].y - 50))
-            {
-               console.log("GOALLL!!");
-               score = score + 1;
-               gameobjects[0].x = 100;
-               gameobjects[0].y = 100;
-            }
-        }
-        
-        if(counter === 10000)
-        {
-            console.log("Time is up");
-            console.log("Stop scoring !!");
-        }
-        else if (counter <= 10000)
-        {
-            counter++;
-        }       
-        if(gameobjects[0].x > 800 || gameobjects[0].y > 800)
-        {
-            resetPosition();
-        }
-}
-function buttonOnClickUp()
-{
-    gameobjects[0].y -= 5;
-    console.log("Up");
-}
-function buttonOnClickDown()
-{
-    gameobjects[0].y += 5;
-    console.log("Down");
-}
-function buttonOnClickLeft()
-{
-    gameobjects[0].x -= 5;
-    console.log("Left");
-}
-function buttonOnClickRight()
-{
-    gameobjects[0].x += 5;
-    console.log("Right");
+    //used for animation
 }
 
 // Draw GameObjects to Console
@@ -167,23 +72,20 @@ function buttonOnClickRight()
 function draw() {
     // Clear Canvas
     // Draw each GameObject
-    context.clearRect(0,0,canvas.width,canvas.height);
-    drawProgressBar(counter/10);
-    
-    context.drawImage(npcSprite , gameobjects[1].x , gameobjects[1].y);
+    context.clearRect(0, 0 ,canvas.width, canvas.height);
+    context.drawImage(backgroundSprite,0,0);
+    drawHealthBar(playerHealth,enemyHealth);
     animate();
 }
-var counter = 0;
 
 var x = 0,
     y = 1300;
 
 // Total Frames
-var frames = 6;
+var frames = 4;
 
 // Current Frame
 var currentFrame = 0;
-
 
 // X axis to Draw from
 var sprite_x = 0;
@@ -199,72 +101,237 @@ function animate() {
         initial = current; // reset initial
     } 
     // Draw sprite frame
-    context.drawImage(sprite, (sprite.width / 6) * currentFrame, 0, 100, 100, gameobjects[0].x, gameobjects[0].y, 100, 100);
+    context.drawImage(sprite, (sprite.width / 4) * currentFrame, 0, 194, 209, gameobjects[0].x, gameobjects[0].y, 100, 100);
 
+    context.drawImage(enemyNPCSprite, (enemyNPCSprite.width / 4) * currentFrame, 0, 230, 176, gameobjects[1].x, gameobjects[1].y, 100, 100);
+
+    console.log(sprite.width);
     context.font = '36pt Orbitron';
 }
 
 window.requestAnimationFrame(gameloop);
+
 function gameloop() {
     update();
     draw();
     window.requestAnimationFrame(gameloop);
 }
 
-// Handle Active Browser Tag Animation
-
-// Handle Keypressed
-window.addEventListener('keyup', input);
-window.addEventListener('keydown', input);
-window.addEventListener('keyleft', input);
-window.addEventListener('keyright', input);
-
-
-function updatePosition() 
-{
-      localStorage.setItem('posX',gameobjects[0].x);
-      document.getElementById("posXText").innerHTML = " [ " + localStorage.getItem('posX') + " ] ";
-//-------------------------------------------------------------------------------------------------------------
-      localStorage.setItem('posY',gameobjects[0].y);
-      document.getElementById("posYText").innerHTML = " [ " + localStorage.getItem('posY') + " ] ";
+function shieldOnClick(){
+     var x = document.getElementById("defButtonDiv");
+     if (x.style.display === "none") {
+        x.style.display = "block";
+        } 
+     else {
+       x.style.display = "none";
+     }
+      console.log("you choose shield which focus on defend");
 }
 
-function getPositionStorage() 
-{
-    var current_posX = localStorage.getItem('posX');
+function bladeOnClick(){
+    var x = document.getElementById("atkButtonDiv");
+    if (x.style.display === "none") {
+       x.style.display = "block";
+       } 
+    else {
+      x.style.display = "none";
+    }
+     console.log("you choose blade which focus on attack");
+}
 
-    if (isNaN(current_posX)) 
+function boxingOnClick(){
+    var x = document.getElementById("counterButtonDiv");
+    if (x.style.display === "none") {
+       x.style.display = "block";
+       } 
+    else {
+      x.style.display = "none";
+    }
+    console.log("you choose boxing which is a balance weapon");
+}
+
+function atkOnClick(){
+    gamerInput = new GamerInput("playerAttack");
+    console.log("gamerInput selected");
+    enemyResponce();  
+}
+
+function defOnClick(){
+    gamerInput = new GamerInput("playerDefence");
+    enemyResponce();
+}
+
+function counterOnClick(){
+    gamerInput = new GamerInput("playerCounter");
+    enemyResponce();
+}
+
+function enemyResponce(){
+    chosenDecision = randomNumberGenerator(2);  
+    setTimeout(enemyAction,1000);
+    setTimeout(damageCalculator,1000);
+}
+
+var damageToEnemy = 0;
+var damageToPlayer = 0;
+
+function damageCalculator(){
+    console.log("damage calculator in 2 seconds");
+    var enemyBlockDmg = 3;
+    var playerBlockDmg = 3;
+
+    if(gamerInput.action == "playerAttack")
     {
-        localStorage.setItem('posX',gameobjects[0].x);
-        document.getElementById("posXText").innerHTML = " [ " + localStorage.getItem('posX') + " ] ";
-      }
-
-      gameobjects[0].x=localStorage.getItem('posX'); 
-      document.getElementById("posXText").innerHTML = " [ " + localStorage.getItem(parseInt('posX')) + " ] ";
-    //---------------------------------------------------------------------------------------------------------
-      var current_posY = localStorage.getItem('posY');
-
-      if (isNaN(current_posY)) 
-      {
-          localStorage.setItem('posY',gameobjects[0].y);
-          document.getElementById("posYText").innerHTML = " [ " + localStorage.getItem('posY') + " ] ";
+        damageToEnemy = 6;
+    }
+    if(enemyInput.action == "enemyAttack")
+    {
+        damageToPlayer = 6;
+        if(gamerInput.action =="playerCounter")
+        {
+            damageToPlayer = 0;
+            damageToEnemy = 6;
         }
+    }
+    if(gamerInput.action == "playerDefence")
+    {
+        damageToEnemy = 0;
+        damageToPlayer - playerBlockDmg;
+    }
+    if(enemyInput.action == "enemyDefence")
+    {
+        damageToPlayer = 0;
+        damageToEnemy - enemyBlockDmg;
+        if(gamerInput.action =="playerCounter")
+        {
+            damageToPlayer = 9;
+            damageToEnemy = 0;
+        }
+    }
+    
+    enemyHealth = enemyHealth - damageToEnemy;
+    playerHealth = playerHealth - damageToPlayer;
+    inCombat = true;
+}
 
-        gameobjects[0].y=localStorage.getItem('posY'); 
-        document.getElementById("posYText").innerHTML = " [ " + localStorage.getItem(parseInt('posY')) + " ] "
+function randomNumberGenerator(maxVal)
+{
+    return Math.floor(Math.random() * Math.floor(maxVal));
+}
 
+function enemyAction(){
+    console.log("enemy action in 2 seconds");
 
+    var enemyAction = chosenDecision;
+    if(enemyAction == 0)
+    {
+        enemyInput = new EnemyInput("enemyDefence");
+    }
+    else if(enemyAction == 1)
+    {
+        enemyInput = new EnemyInput("enemyAttack");
+    }
+    
+}
+
+var chosenDecision = 0;
+
+function enemyDecision(){
+ 
+    context.font ="15px Arial";
+    context.textAlign = "center";
+    context.fillStyle = "#000000";  
+    context.fillRect(0, 0, 400, 20);
+
+    if(chosenDecision == 1)
+    {    
+        context.fillStyle ="red";
+        context.fillText("Enemy choose to attack " , 150 , 15);
+    }    
+    else 
+    {
+        context.fillStyle ="blue";
+        context.fillText("Enemy choose to defend " , 150 , 15);
+    }
+}
+
+var inCombat = false;
+
+var enemyHealth = 100;
+var playerHealth = 100;
+
+var counter = 180;
+
+function drawHealthBar(playerHealth,enemyHealth) {
+    var width = 100;
+    var height = 10;
+    var max = 100;
+
+    // Draw the background
+    context.fillStyle = "#000000";  
+    context.fillRect(20, 20, width, height);
+  
+    // Draw the fill
+    context.fillStyle = "#7CFC00";
+    var fillVal = Math.min(Math.max(playerHealth / max, 0), 1);
+    context.fillRect(20, 20, fillVal * width, height);
+
+    context.fillStyle = "#000000";
+    context.fillRect(170, 20, width, height);
+  
+    // Draw the fill
+    context.fillStyle = "#7CFC00";
+    fillVal = Math.min(Math.max(enemyHealth / max, 0), 1);
+    context.fillRect(170, 20, fillVal * width, height);
+
+    if(inCombat == true)
+    {
+        enemyDecision();
+        if(counter <= 0)
+        {
+            inCombat = false;
+            counter = 180;
+        }
+        else
+        {
+            counter--;
+        }
+    }   
+}
+
+function updateHealthBar()
+{
+    localStorage.setItem('playerCurHealth' , playerHealth);
+    document.getElementById("playerHealthText").innerHTML = " [ " + localStorage.getItem('playerCurHealth') + " ] ";
+    //============================================================================================================
+    localStorage.setItem('enemyCurHealth' , enemyHealth);
+    document.getElementById("enemyHealthText").innerHTML = " [ " + localStorage.getItem('enemyCurHealth') + " ] ";
+}
+
+function getHealthStorage()
+{
+    var currentPlayerHealth = localStorage.getItem('playerCurHealth');
+
+    if(isNaN(currentPlayerHealth))
+    {
+        localStorage.setItem('playerCurHealth',playerHealth);
+        document.getElementById("playerHealthText").innerHTML = " [ " + localStorage.getItem('playerCurHealth') + " ] ";
+    }
+    playerHealth = localStorage.getItem('playerCurHealth');
+    document.getElementById("playerHealthText").innerHTML = " [ " + localStorage.getItem(parseInt('playerCurHealth')) + " ] ";
+    //========================================================================================================================
+    var currentEnemyHealth = localStorage.getItem('enemyCurHealth');
+
+    if(isNaN(currentEnemyHealth))
+    {
+        localStorage.setItem('enemyCurHealth',playerHealth);
+        document.getElementById("enemyHealthText").innerHTML = " [ " + localStorage.getItem('enemyCurHealth') + " ] ";
+    }
+    enemyHealth = localStorage.getItem('enemyCurHealth');
+    document.getElementById("enemyHealthText").innerHTML = " [ " + localStorage.getItem(parseInt('enemyCurHealth')) + " ] ";
 }
 
 function onPageLoad()
 {
-    getPositionStorage();
-}
-
-function resetPosition()
-{
-    localStorage.setItem('posX',0); 
-    gameobjects[0].x=0;
-    localStorage.setItem('posY',0);
-    gameobjects[0].y=0;
+    getHealthStorage();
 }
